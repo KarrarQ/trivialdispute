cf16fd452922ac6820848e92f89da3bdb316575d
Karrar Qasim <Karawr1996@gmail.com>
2023-03-01 16:25:07 -0700

    Added basic logic for form component

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,17 +1,17 @@
 export const apiCalls = {
 
     async getAllCategories() {
-      let response = await fetch('http://localhost:3001/api/v1/questions/all')
+      let response = await fetch('http://localhost:3000/api/v1/questions/all')
       return await response.json();
     },
   
     async getQuestionsByCategory(category) {
-      let response = await fetch(`http://localhost:3001/api/v1/questions?category=${category}`)
+      let response = await fetch(`http://localhost:3000/api/v1/questions?category=${category}`)
       return await response.json()
     },
   
     async postNewQuestion(question) {
-      const response = await fetch('http://localhost:3001/api/v1/questions', {
+      const response = await fetch('http://localhost:3000/api/v1/questions', {
         method: 'POST',
         body: JSON.stringify(question),
         headers: {
