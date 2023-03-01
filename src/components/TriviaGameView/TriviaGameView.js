f3f077f132f1be513afdfdf067e66a7b56e2ee72
Karrar Qasim <Karawr1996@gmail.com>
2023-03-01 14:33:50 -0700

    Added a component did mount

--- src/components/TriviaGameView/TriviaGameView.js
+++ src/components/TriviaGameView/TriviaGameView.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { apiCalls } from '../../apiCalls'
-import QuestionCard from '../QuestionCard/QuestionCard';
+import GameViewContainer from '../GameViewContainer/GameViewContainer';
 
-const GameViewContainer = ({ questions }) => {
-  let i = 0;
-  console.log(questions)
-  const questionCards = questions.map((question) => {
-    console.log(question)
-    return (
-      <QuestionCard
-        key={i++}
-        question={question.question}
-        incorrectAnswers={question.incorrect_answers}
-        correctAnswer={question.correct_answer}
-        id={question.id}
-      />
-    )
-  })
-  return (
-    <section className='game-view-container'>
-      {questionCards}
-    </section>
-  )
+class TriviaGameView extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        selectedCategory: [],
+        error: ''
+      }
+    }
+  
+    async componentDidMount() {
+      console.log(this.props);
+      console.log('we mounted')
+      const category = this.props.category;
+      try {
+        const data = (category !== 'All Movies') ? await apiCalls.getQuestionsByCategory(category) : await apiCalls.getAllQuestions();
+        let questions = data;
+        console.log(questions)
+        this.setState({ selectedCategory: questions })
+      }
+      catch (error) {
+        this.setState({ error: error.message })
+      }
+    }
 
+    render() {
+        console.log(this.state.selectedCategory)
+        return (
+          <section className='trivia-game-card'>
+            <GameViewContainer questions={this.state.selectedCategory}/>
+          </section>
+        )
+      }
 }
-
-export default GameViewContainer;
+    
+export default TriviaGameView;
